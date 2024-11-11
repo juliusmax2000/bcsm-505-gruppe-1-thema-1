5edbfba4f1f6bbe39977d83e6fb24264c0f6d5da
code-server <code-server@example.com>
2024-11-11 13:03:05 +0100

    Implement enable/disable variable for button (Julius)

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ define('MAX_FILE_SIZE', 5 * 1024 * 1024); // 5MB in Bytes
 define('UPLOAD_DIR', 'uploads/');
 define('DATA_FILE', 'Datenhalde.json');
 
+// Variable for enable/disable submit button
+$is_button_enabled = true;
+
 // Arrays for dropdown menus
 $stellentypen = ["Jobs", "Praxisphasen & Praktika", "Abschlussarbeiten", "Werkstudentenstellen", "Traineestellen", "Studentische Hilfskräfte", "Tutorentätigkeit", "Jobs im Ausland", "Promotionen", "Nebenjobs (in der Region)", "Praktika", "Sonstiges"];
 $fachbereiche = ["Fachbereich 01 Chemie", "Fachbereich 02 Design", "Fachbereich 03 Elektrotechnik und Informatik", "Fachbereich 04 Maschinenbau und Verfahrenstechnik", "Fachbereich 05 Oecotrophologie", "Fachbereich 06 Sozialwesen", "Fachbereich 07 Textil- und Bekleidungstechnik", "Fachbereich 08 Wirtschaftswissenschaften", "Fachbereich 09 Wirtschaftsingenieurwesen", "Fachbereich 10 Gesundheitswesen"];
@@ -234,7 +237,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             setInterval(resetTurnstile, 30000);
         </script>
 
-        <button type="submit">Hochladen</button>
+        <button type="submit" <?php echo $is_button_enabled ? '' : 'disabled'; ?>>Hochladen</button>
     </form>
 </body>
 
